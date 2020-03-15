05072dd28dd8f370c3dc9a7f7d5847b1b06cdf71
JasonParkerX <s659wang@edu.uwaterloo.ca>
2020-03-15 04:22:20 -0400

    Improve the code structure and update the UI

--- Jalculator/MainView/MainView.swift
+++ Jalculator/MainView/MainView.swift
@@ -8,18 +8,8 @@
 
 import SwiftUI
 
-struct ButtonTextView: View {
-    var title: String
-    var isBold: Bool
-    var color: Color
-    var body: some View {
-        if isBold {
-            return Text(title).font(.largeTitle).foregroundColor(color).bold()
-        } else {
-            return Text(title).font(.largeTitle).foregroundColor(color)
-        }
-    }
-}
+let buttonTapSoundEffectPlayer = ButtonTapSoundEffect()
+let buttonSize = UIScreen.main.bounds.width > 414 ? 100 : UIScreen.main.bounds.width * 0.75 / 4
 
 struct MainView: View {
     
@@ -44,7 +34,16 @@ struct MainView: View {
         var operations = [String]()
     }
     
+    struct ButtonAttributes {
+        var id = UUID()
+        var text: String
+        var textColor: UIColor
+        var backgroundColor: UIColor
+        var action: () -> ()
+    }
+    
     let operationSymbols = ["+", "-", "×", "÷"]
+    
     @State var clearButtonTitle = "AC"
     @State var displayString: String = " " {
         willSet {
@@ -84,23 +83,24 @@ struct MainView: View {
             }
         }
     }
+    
     @EnvironmentObject var settings: Settings
-    let buttonTapSoundEffectPlayer = ButtonTapSoundEffect()
     
     func pressClear() {
         appState = .Clear
     }
     
-    func pressNumber(number: String) {
+    func pressNumber(number: Int) {
+        let numberString = String(number)
         switch appState {
         case .Clear:
             clearButtonTitle = "C"
-            displayString = number
+            displayString = numberString
         case .Number, .DecimalPoint, .Operation:
-            displayString += number
+            displayString += numberString
         case .Equal:
             appState = isDecimalState(expression: displayString) ? .Number(true) : .Number(false)
-            displayString += number
+            displayString += numberString
         default:
             return
         }
@@ -179,7 +179,6 @@ struct MainView: View {
             }
             let expression = NSExpression(format: expressionString)
             let tempResult = (expression.expressionValue(with: nil, context: nil) as! NSNumber).doubleValue
-            print(tempResult)
             if tempResult.isInfinite || tempResult.isNaN {
                 appState = .Error
                 return
@@ -200,223 +199,129 @@ struct MainView: View {
     }
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text(displayString)
-                .font(.system(size: 40))
-                .bold()
-                .foregroundColor(Color(UIColor.label))
-                .lineLimit(1)
-                .minimumScaleFactor(0.6)
-                .padding(.leading, 40)
-                .padding(.trailing, 40)
-            Spacer()
+        
+        let buttonAttributes = [
+            [
+                ButtonAttributes(text: "D", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressDelete() }),
+                ButtonAttributes(text: "+/-", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressToggle() }),
+                ButtonAttributes(text: "√x", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressToggle() }),
+                ButtonAttributes(text: clearButtonTitle, textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressClear() }),
+            ],
+            [
+                ButtonAttributes(text: "7", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 7) }),
+                ButtonAttributes(text: "8", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 8) }),
+                ButtonAttributes(text: "9", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 9) }),
+                ButtonAttributes(text: "÷", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressOperation(operation: .divide) }),
+            ],
+            [
+                ButtonAttributes(text: "4", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 4) }),
+                ButtonAttributes(text: "5", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 5) }),
+                ButtonAttributes(text: "6", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 6) }),
+                ButtonAttributes(text: "×", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressOperation(operation: .multiply) }),
+            ],
+            [
+                ButtonAttributes(text: "1", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 1) }),
+                ButtonAttributes(text: "2", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 2) }),
+                ButtonAttributes(text: "3", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 3) }),
+                ButtonAttributes(text: "+", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressOperation(operation: .add) }),
+            ],
+            [
+                ButtonAttributes(text: ".", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressDecimalPoint() }),
+                ButtonAttributes(text: "0", textColor: .systemBackground, backgroundColor: .label, action: { self.pressNumber(number: 0) }),
+                ButtonAttributes(text: "=", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressEqual() }),
+                ButtonAttributes(text: "-", textColor: .OperationButtonTextColor, backgroundColor: .OperationButtonBackgroundColor, action: { self.pressOperation(operation: .minus) }),
+            ]
+        ]
+        
+        return (
             VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressDelete()
-                    }) {
-                        ButtonTextView(title: "D", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressToggle()
-                    }) {
-                        ButtonTextView(title: "+/-", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressRoot()
-                    }) {
-                        ButtonTextView(title: "√a", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressClear()
-                    }) {
-                        ButtonTextView(title: clearButtonTitle, isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                }.padding(.bottom, 20)
-                HStack {
-                    Spacer()
-                    ForEach(["7", "8", "9"], id: \.self) { title in
-                        Group {
-                            Button(action: {
-                                self.buttonTapSoundEffectPlayer.play()
-                                self.pressNumber(number: title)
-                            }) {
-                                ButtonTextView(title: title, isBold: true, color: Color(UIColor.systemBackground))
-                            }
-                            .frame(width: 80, height: 80)
-                            .background(Color(UIColor.label))
-                            .cornerRadius(100)
-                            Spacer()
-                        }
-                    }
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressOperation(operation: .divide)
-                    }) {
-                        ButtonTextView(title: Operation.divide.rawValue, isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                }.padding(.bottom, 20)
-                HStack {
-                    Spacer()
-                    ForEach(["4", "5", "6"], id: \.self) { title in
-                        Group {
-                            Button(action: {
-                                self.buttonTapSoundEffectPlayer.play()
-                                self.pressNumber(number: title)
-                            }) {
-                                ButtonTextView(title: title, isBold: true, color: Color(UIColor.systemBackground))
-                            }
-                            .frame(width: 80, height: 80)
-                            .background(Color(UIColor.label))
-                            .cornerRadius(100)
-                            Spacer()
-                        }
-                    }
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressOperation(operation: .multiply)
-                    }) {
-                        ButtonTextView(title: Operation.multiply.rawValue, isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                }.padding(.bottom, 20)
-                HStack {
-                    Spacer()
-                    ForEach(["1", "2", "3"], id: \.self) { title in
-                        Group {
-                            Button(action: {
-                                self.buttonTapSoundEffectPlayer.play()
-                                self.pressNumber(number: title)
-                            }) {
-                                ButtonTextView(title: title, isBold: true, color: Color(UIColor.systemBackground))
-                            }
-                            .frame(width: 80, height: 80)
-                            .background(Color(UIColor.label))
-                            .cornerRadius(100)
-                            Spacer()
-                        }
-                    }
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressOperation(operation: .add)
-                    }) {
-                        ButtonTextView(title: Operation.add.rawValue, isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                }.padding(.bottom, 20)
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressDecimalPoint()
-                    }) {
-                        ButtonTextView(title: ".", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressNumber(number: "0")
-                    }) {
-                        ButtonTextView(title: "0", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.label))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressEqual()
-                    }) {
-                        ButtonTextView(title: "=", isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                    Button(action: {
-                        self.buttonTapSoundEffectPlayer.play()
-                        self.pressOperation(operation: .minus)
-                    }) {
-                        ButtonTextView(title: Operation.minus.rawValue, isBold: true, color: Color(UIColor.systemBackground))
-                    }
-                    .frame(width: 80, height: 80)
-                    .background(Color(UIColor.systemGray))
-                    .cornerRadius(100)
-                    Spacer()
-                }.padding(.bottom, 20)
-            }
-            HStack {
                 Spacer()
-                Button(action: {
-                    self.isSettingsViewPresented.toggle()
-                }) {
-                    Image("settingsIcon")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(Color(UIColor.label))
+                Text(displayString)
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(Color(UIColor.label))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.6)
+                    .padding(.leading, 40)
+                    .padding(.trailing, 40)
+                Spacer()
+                VStack {
+                    ForEach(buttonAttributes, id: \.first!.id, content: { row in
+                        HStack {
+                            Group {
+                                ForEach(row, id: \.id, content: { column in
+                                    Group {
+                                        Spacer()
+                                        ButtonView(action: column.action, title: column.text, textColor: column.textColor, backgroundColor: column.backgroundColor)
+                                    }
+                                })
+                                Spacer()
+                            }
+                        }.padding(.bottom, 20)
+                    })
                 }
-                .sheet(isPresented: $isSettingsViewPresented, content: {
-                    SettingsView().environmentObject(self.settings)
-                })
-                Spacer()
-                Button(action: {
-                    self.isInfoViewPresented.toggle()
-                }) {
-                    ZStack {
-                        Image("questionBackgroundIcon")
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.isSettingsViewPresented.toggle()
+                    }) {
+                        Image("settingsIcon")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 20, height: 20)
                             .foregroundColor(Color(UIColor.label))
-                        Image("questionIcon")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color(UIColor.systemBackground))
                     }
-                }
-                .sheet(isPresented: $isInfoViewPresented, content: {
-                    InfoView()
-                })
-                Spacer()
-            }.padding(.bottom, 20)
-        }
+                    .sheet(isPresented: $isSettingsViewPresented, content: {
+                        SettingsView().environmentObject(self.settings)
+                    })
+                    Spacer()
+                    Button(action: {
+                        self.isInfoViewPresented.toggle()
+                    }) {
+                        ZStack {
+                            Image("questionBackgroundIcon")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color(UIColor.label))
+                            Image("questionIcon")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(Color(UIColor.systemBackground))
+                        }
+                    }
+                    .sheet(isPresented: $isInfoViewPresented, content: {
+                        InfoView()
+                    })
+                    Spacer()
+                }.padding(.bottom, 20)
+            }
+        )
+    }
+}
+
+struct ButtonView: View {
+    var action: () -> ()
+    var title: String
+    var textColor: UIColor
+    var backgroundColor: UIColor
+    var body: some View {
+        return (
+            Button(action: {
+                self.action()
+                buttonTapSoundEffectPlayer.play()
+            }, label: {
+                Text(title)
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color(textColor))
+                    .frame(width: buttonSize, height: buttonSize)
+                    .mask(Circle())
+            })
+                .background(Color(backgroundColor))
+                .mask(Circle())
+        )
     }
 }
 
@@ -424,4 +329,16 @@ struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
     }
+}
+
+extension UIColor {
+    
+    static var OperationButtonBackgroundColor: UIColor {
+        return UIColor(named: "OperationButtonBackgroundColor")!
+    }
+    
+    static var OperationButtonTextColor: UIColor {
+        return UIColor(named: "OperationButtonTextColor")!
+    }
+    
 }
