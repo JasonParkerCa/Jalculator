14b3e8a989aaf9e4d60eae540fb52b8276d5a449
Shenjie Wang <s659wang@uwaterloo.ca>
2020-05-31 15:04:26 -0400

    Add Expression

---
+++ Jalculator/MainView/Expression.swift
@@ -0,0 +1,66 @@
+//
+//  Expression.swift
+//  Jalculator
+//
+//  Created by Jason Parker on 2020-05-31.
+//  Copyright © 2020 Jason Parker. All rights reserved.
+//
+
+import Foundation
+
+class Expression {
+    
+    var expressionString: String
+    
+    init(expressionString: String) {
+        self.expressionString = expressionString
+    }
+    
+    func calculate() -> Decimal {
+        process(mode: "*/")
+        process(mode: "+-")
+        expressionString.remove(at: expressionString.startIndex)
+        expressionString.remove(at: expressionString.index(before: expressionString.endIndex))
+        return Decimal(string: expressionString)!
+    }
+    
+    func process(mode: String) {
+        let regex_subExpression = mode == "*/" ? "\\([0-9|(e\\+)|\\-|\\.]+\\)(\\*|\\/)\\([(0-9|(e\\+)|\\-|\\.]+\\)" : "\\([0-9|(e\\+)|\\-|\\.]+\\)(\\+|\\-)\\([(0-9|(e\\+)|\\-|\\.]+\\)"
+        let regex_operation = mode == "*/" ? "(\\*|\\/)(?=\\()" : "(\\+|\\-)(?=\\()"
+        let regex_number = "([0-9]|(e\\+)|\\-|\\.){3,}"
+        while true {
+            if let subExpressionRange = expressionString.range(of: regex_subExpression, options: .regularExpression, range: nil, locale: nil) {
+                let subExpression = String(expressionString[subExpressionRange])
+                let numbers = matches(for: regex_number, in: subExpression)
+            let operation = subExpression[subExpression.range(of: regex_operation, options: .regularExpression, range: nil, locale: nil)!]
+                var result: Decimal = 0
+                if operation == "*" {
+                    result = Decimal(string: numbers[0])! * Decimal(string: numbers[1])!
+                } else if operation == "/" {
+                    result = Decimal(string: numbers[0])! / Decimal(string: numbers[1])!
+                } else if operation == "+" {
+                    result = Decimal(string: numbers[0])! + Decimal(string: numbers[1])!
+                } else {
+                    result = Decimal(string: numbers[0])! - Decimal(string: numbers[1])!
+                }
+                expressionString.replaceSubrange(subExpressionRange, with: "(\(result))")
+            } else {
+                return
+            }
+        }
+    }
+    
+    func matches(for regex: String, in text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+}
